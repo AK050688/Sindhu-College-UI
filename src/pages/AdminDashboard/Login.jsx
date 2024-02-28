0235b6f23ef76e736cd665e2dc1ba0d6677d69f5
Ayushi Vashisth <ayushivashisth22@gmail.com>
2024-02-29 00:06:54 +0530

    AyushiVashisth

--- src/pages/AdminDashboard/Login.jsx
+++ src/pages/AdminDashboard/Login.jsx
@@ -61,13 +61,13 @@ function Login() {
     <div
       className="text-white h-[100vh] flex justify-center items-center bg-cover bg-login"
     >
-      <div className="bg-slate-800 border border-slate-400 rounded-md p-10 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
+      <div className="bg-slate-800 border border-slate-400 rounded-md p-16 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-10 relative">
         <h1 className="text-4xl text-white font-bold text-center mb-6">
           Login
         </h1>
         <form action="" onSubmit={handleSubmit}>
           <div className="relative my-4">
-            <label htmlFor="username" className="block text-md text-white mb-2">
+            <label htmlFor="username" className="block text-md text-white mb-2 text-left">
               Username
             </label>
             <input
@@ -75,7 +75,7 @@ function Login() {
               id="username"
               name="userName"
               value={userName}
-              className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-1  border-white appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+              className="block w-72 py-2.3 px-0 text-sm text-white border-1  border-white bg-white appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
               onChange={handleChange}
             />
             <FaUserCircle className="absolute top-0 right-1" />
@@ -89,20 +89,20 @@ function Login() {
               id="password"
               name="password"
               value={password}
-              className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+              className="block w-72 py-[5%] rounded-lg px-0 text-sm text-white border-1  border-white bg-white appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
               onChange={handleChange}
             />
             <IoLockOpenOutline className="absolute top-0 right-1" />
           </div>
           <div className="flex justify-between items-center ">
             <div className="flex gap-2 items-center">
-              <input type="checkbox" />
+              <input type="checkbox" className="-mt-2"/>
               <label htmlFor="remember"> Remember me</label>
             </div>
-            <span className="text-blue-500">Forgot password?</span>
+            <span className="text-blue-500 -mt-2">Forgot password?</span>
           </div>
           <button
-            className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-slate-600 hover:bg-slate-500 hover:text-white py-2 transition-colors duration-300"
+            className="w-full mb-4 text-[18px] mt-6 rounded-full bg-yellow-400 text-black hover:bg-yellow-200 hover:text-black py-2 transition-colors duration-300"
             type="submit"
           >
             {loading ? (
